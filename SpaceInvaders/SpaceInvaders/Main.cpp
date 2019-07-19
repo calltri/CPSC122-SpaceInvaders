e0a4818643e57153e36a5cd709500179852c56ab
Tristan <tristan.call122@gmail.com>
2019-07-18 21:44:27 -0700

    Added SFML files and set up program to work with them

--- SpaceInvaders/SpaceInvaders/Main.cpp
+++ SpaceInvaders/SpaceInvaders/Main.cpp
@@ -1,8 +1,23 @@
-
+#include"SFML/Graphics.hpp"
 
 int main()
 {
+	sf::RenderWindow gameWindow(sf::VideoMode(640, 800), "CPTS 122 Lab", sf::Style::Titlebar | sf::Style::Close);
+
+	gameWindow.setVerticalSyncEnabled(true); //matches frame rate of this to computer
 
 
-	return 0;
+	while (gameWindow.isOpen())
+	{
+		sf::Event myevent;
+
+		while (gameWindow.pollEvent(myevent))
+		{
+			if (myevent.type == sf::Event::Closed)
+				gameWindow.close();
+		}
+
+		gameWindow.clear();
+		gameWindow.display();
+	}
 }
